f9bf7330ad6717de9991eae9a872367b3a6ecd9e
kortsa <leoklomegah8@gmail.com>
2024-05-09 16:55:16 +0000

    Learning how to use material UI in reactJs

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import SearchAppBar from "./Components/AppBar";
+import TourCard from "./Components/TourCard";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import cities from "./data.json";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              key={city.id}
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+
+            <Grid container spacing={5}>
+              {city.tours.map((tour, id) => (
+                <TourCard tour={tour} key={id} />
+              ))}
+            </Grid>
+          </>
+        ))}
+      </Container>
+    </div>
+  );
+}
+
+export default App;
